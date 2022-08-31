672871ba6aaed56f76f85a5b8c40f0d05238e018
ploew <patrick.loew@smail.th-koeln.de>
2022-08-31 23:39:29 +0200

    adding nemo Punctuation and Capitalization service

--- astAV.py
+++ astAV.py
@@ -1,10 +1,12 @@
 from src.boundary.stask import Stask
+from src.formator.impl import Txt, Srt
 from src.gui.gui import astAVGui
 from src.translator.service.itranslatortask import ITranslatorTask
 from src.translator.impl.deepspeechimpl import DeepspeechImpl
 from src.translator.impl.voskimpl import VoskImpl
 from src.formator.service.iformatortask import IFormatorTask
 from src.formator.impl import *
+from src.corrector.impl.nemopc import NemoPC
 
 from src.backend.impl.scheduler import Scheduler
 
@@ -54,6 +56,23 @@ class test:
             translator=VoskImpl,
             translatorparam={"modellocation": "speechmodels/vosk/en/vosk_us"},
             formator=Srt,
+        )
+        Scheduler.insertTask(task)
+        Scheduler.startTranscription()
+
+    def vosktextpc(self):
+        translator = ITranslatorTask.__subclasses__()
+        formator = IFormatorTask.__subclasses__()
+        task = Stask(
+            filelocation="Audio_zone-London_life.mp3",
+            filename="Audio_zone-London_life_vosktext_char120",
+            writelocation="",
+            translator=VoskImpl,
+            translatorparam={"modellocation": "speechmodels/vosk/en/vosk_us"},
+            formator=Txt,
+            formatorparam={"textmode" : "char120"},
+            corrector=NemoPC,
+            correctorparam={"modellocation": "speechmodels/nemopc/punctuation_en_bert.nemo"}
         )
         Scheduler.insertTask(task)
         Scheduler.startTranscription()
---
+++ src/corrector/impl/nemopc.py
@@ -0,0 +1,50 @@
+from src.boundary.guiparam.guiparam import GuiParam, GuiParamFile
+from src.boundary.phrasetoken import PhraseToken
+from src.boundary.stask import Stask
+from src.corrector.service.icorrectorguiparam import ICorrectorGuiParam
+from src.corrector.service.icorrectortask import ICorrectorTask
+from nemo.collections.nlp.models import PunctuationCapitalizationModel
+
+import gettext
+
+_ = gettext.gettext
+
+class NemoPC(ICorrectorTask, ICorrectorGuiParam):
+    __task : Stask = []
+    __model : PunctuationCapitalizationModel =[]
+
+    def __init__(self, task : Stask):
+        self.__task = task
+        self.__model = PunctuationCapitalizationModel.restore_from(task.correctorparam.get("modellocation"))
+
+    def correctText(self, textcandidat : PhraseToken) -> PhraseToken:
+        rawtext = textcandidat.getText()
+        inference_text = self.__model.add_punctuation_capitalization([rawtext], max_seq_length=128, return_labels=True)[0]
+
+        text_list = []
+        for (inference_word, textcandidat_word) in zip(inference_text.split(" "), textcandidat.splitInToWords()):
+            textcandidat_word : PhraseToken
+            if(inference_word[0] != "O"):
+               textcandidat_word.insertAtPos(len(textcandidat_word.chartokenlist), inference_word[0])
+            if(inference_word[1] == "U"):
+                textcandidat_word.chartokenlist[0].char = textcandidat_word.chartokenlist[0].char.upper()
+            text_list.append(textcandidat_word)
+        return PhraseToken(text_list)
+
+    @staticmethod
+    def getNeededParams() -> [GuiParam]:
+        modelparam = GuiParamFile()
+        modelparam.displayname = _("KI-Model")
+        modelparam.name = "modellocation"
+        modelparam.defvalue = ""
+        modelparam.mouesover = _("Die Datei in dem sich das KI-Model für Zeichensetzung und Groß- und Kleinschreibung befinget." +
+                                 "Das Model muss auf die Sprache angepasst sein.")
+        return [modelparam]
+
+    @staticmethod
+    def getName() -> str:
+        return _("Nemo PC")
+
+    @staticmethod
+    def getDescription() -> str:
+        return _("Korrigiert Groß- und Kleinschreibung fügt Punkte hinzu")
