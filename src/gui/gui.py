5aa0102ed8f8ad4fcf70b9c1583e4d9878f0293e
ploew <john@dohe.com>
2023-09-27 13:51:37 +0200

    integrate pannel in gui

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -9,7 +9,15 @@ from src.gui.render.renderscreen import RenderScreen
 from kivy.lang import Builder
 from kivy.uix.settings import SettingsWithSidebar
 from kivy.uix.settings import SettingOptions
+from kivy.uix.settings import SettingsPanel
+from kivy.uix.settings import ContentPanel
+from kivy.config import ConfigParser
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from src.config.configsupply import ConfigSupply
+from src.gui.settings.newpreset import NewPreset
+from src.gui.settings.managepreset import ManagePreset
 from gettext import gettext
 
 _ :gettext
@@ -23,6 +31,10 @@ class astAVGui(App):
         Builder.load_file("src/gui/main/hauptscreen.kv")
         Builder.load_file("src/gui/task/taskscreen.kv")
         Builder.load_file("src/gui/render/rendersreen.kv")
+
+        Builder.load_file("src/gui/settings/newpreset.kv")
+        Builder.load_file("src/gui/settings/managepreset.kv")
+
         sm = ScreenManager()
         sm.switch_to(HauptScreen(name="Übersicht"))
         return sm
@@ -42,23 +54,33 @@ class astAVGui(App):
         new_taskscreen = TaskScreen(name="Neue Aufgabe")
         app.root.switch_to(new_taskscreen)
 
-    def build_config(self, config):
+    def build_config(self, config: ConfigParser):
         lang_config = ConfigSupply()
         config.setdefaults('language', {
             'currentlanguage': lang_config.getLaguageList()[1]})
 
-    def build_settings(self, settings):
-        #option = SettingOptions(section="example", key="optionsexample", options=["option1", "option2", "option3"])
+    def build_settings(self, settings: SettingsWithSidebar):
         lang_config = ConfigSupply()
+
+        managerpreset_pannel = ManagePreset()
+        managerpreset_pannel.setup()
+        settings.interface.add_panel(managerpreset_pannel, _("manage_presets"), managerpreset_pannel.uid)
+
+        newpreset_pannel = NewPreset()
+        newpreset_pannel.setupWithCallback(managerpreset_pannel.refreshCallback)
+
+        settings.interface.add_panel(newpreset_pannel, _("new_preset"), newpreset_pannel.uid)
+
         settings.add_json_panel(_('Sprache'),
                                 self.config,
-                                data=json.dumps([{'type': 'options', 'title': _('Sprachauswahl'), 'desc': 'Neustart erforderlich', 'section': 'language','key': 'currentlanguage','options': lang_config.getLaguageList()[0]}]))
-
+                                data=json.dumps([{'type': 'options', 'title': _('Sprachauswahl'), 'section': 'language','key': 'currentlanguage','options': lang_config.getLaguageList()[0]}]))
 
     def on_config_change(self, config, section,
                          key, value):
         if (key == 'currentlanguage'):
             ConfigSupply().setCurrentLanguage(value)
+            self.switchToMain()
+
 
     def get_application_config(self):
         return super(astAVGui, self).get_application_config(
